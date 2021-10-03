98ae83538700ce5b9bff9b3f07461249e5ba3ec5
Frank Riccobono <F.Riccobono@gmail.com>
2021-10-03 13:33:26 -0400

    Fully implement JavaFX version...still some warts

--- src/main/java/com/frankriccobono/FXMLController.java
+++ src/main/java/com/frankriccobono/FXMLController.java
@@ -1,5 +1,6 @@
 package com.frankriccobono;
 
+import com.frankriccobono.github.GithubApiWrapper;
 import com.frankriccobono.github.Repository;
 import com.frankriccobono.github.RepositoryService;
 import javafx.collections.FXCollections;
@@ -9,83 +10,149 @@ import javafx.concurrent.Worker;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
-import javafx.scene.control.ListView;
-import javafx.scene.control.SelectionMode;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.text.Text;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.Optional;
 
 public class FXMLController {
-  private final RepositoryService service = new RepositoryService();
-  File destinationDir;
-  @FXML
-  ListView<Repository> repoListView;
+    private final RepositoryService service = new RepositoryService();
+    File destinationDir;
+    @FXML
+    ListView<Repository> repoListView;
 
-  private ObservableList<Repository> repositories = FXCollections.observableArrayList();
+    @FXML
+    Text selectedDirectory;
 
-  private FilteredList<Repository> filteredList;
+    @FXML
+    ProgressBar progressBar;
 
-  public void initialize() {
-    service.stateProperty().addListener(
-        observable -> {
-          Worker.State now = service.getState();
-          System.out.println("State of service = " + now);
-          if (now == Worker.State.SUCCEEDED) {
-            repositories.setAll(service.getValue());
-          }
+    private final ObservableList<Repository> repositories = FXCollections.observableArrayList();
+
+    private FilteredList<Repository> filteredList;
+
+    public void initialize() {
+        service.stateProperty().addListener(
+            observable -> {
+                Worker.State now = service.getState();
+                System.out.println("State of service = " + now);
+                if (now == Worker.State.SUCCEEDED) {
+                    repositories.setAll(service.getValue());
+                }
+            }
+        );
+        service.start();
+
+        filteredList = new FilteredList<>(repositories, s -> true);
+        repoListView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+        repoListView.setItems(filteredList);
+        repoListView.setCellFactory(view -> new App.RepositoryCell());
+    }
+
+    @FXML
+    protected void handleUpdateFilter(KeyEvent event) {
+        final TextField source = (TextField) event.getSource();
+        final String filter = source.getText();
+        filteredList.setPredicate(
+            s -> filter == null ||
+                filter.length() == 0 ||
+                s.full_name.contains(filter));
+    }
+
+    @FXML
+    protected void handleDirectoryChange(ActionEvent event) {
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+        destinationDir = directoryChooser.showDialog(stage);
+        this.selectedDirectory.setText(destinationDir.getAbsolutePath());
+    }
+
+    @FXML
+    protected void handleRefresh(ActionEvent event) {
+        service.restart();
+    }
+
+    @FXML
+    protected void handleClone(ActionEvent event) {
+        ObservableList<Repository> selectedItems =
+            repoListView.getSelectionModel().getSelectedItems();
+        int total = selectedItems.size();
+        double count = 0.0;
+        for (Repository repo : selectedItems) {
+            cloneOrPullRepo(repo.sshUrl, repo.name);
+            count++;
+            progressBar.setProgress(count / total);
         }
-    );
-    service.start();
-
-    filteredList = new FilteredList<>(repositories, s -> true);
-    repoListView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
-    repoListView.setItems(filteredList);
-    repoListView.setCellFactory(view -> new App.RepositoryCell());
-  }
-
-  @FXML
-  protected void handleUpdateFilter(KeyEvent event) {
-    final TextField source = (TextField) event.getSource();
-    final String filter = source.getText();
-    filteredList.setPredicate(
-        s -> filter == null ||
-            filter.length() == 0 ||
-            s.full_name.contains(filter));
-  }
-
-  @FXML
-  protected void handleDirectoryChange(ActionEvent event) {
-    Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-    DirectoryChooser directoryChooser = new DirectoryChooser();
-    destinationDir = directoryChooser.showDialog(stage);
-  }
-
-  @FXML
-  protected void handleRefresh(ActionEvent event) {
-    final Repository repo1 = new Repository();
-    repo1.full_name = "Refresh " + System.currentTimeMillis();
-    repositories.add(repo1);
-
-  }
-
-  @FXML
-  protected void handleClone(ActionEvent event) {
-    ObservableList<Repository> selectedItems =
-        repoListView.getSelectionModel().getSelectedItems();
-    for (Repository repo : selectedItems) {
-      System.out.println(repo);
+        progressBar.setProgress(1.0);
     }
-  }
 
-  @FXML
-  protected void handleDelete(ActionEvent event) {
-    ObservableList<Repository> selectedItems =
-        repoListView.getSelectionModel().getSelectedItems();
-    for (Repository repo : selectedItems) {
-      System.out.println(repo);
+    @FXML
+    protected void handleDelete(ActionEvent event) {
+        ObservableList<Repository> selectedItems =
+            repoListView.getSelectionModel().getSelectedItems();
+        int total = selectedItems.size();
+        double count = 0.0;
+        for (Repository repo : selectedItems) {
+
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setTitle("Confirmation Dialog");
+            alert.setHeaderText("Deleting a Repository");
+            alert.setContentText("Are you sure you want to delete " + repo.full_name + "?");
+
+            ButtonType delete = new ButtonType("Delete");
+            ButtonType cancel = new ButtonType("Cancel");
+
+            alert.getButtonTypes().setAll(delete, cancel);
+
+            Optional<ButtonType> result = alert.showAndWait();
+
+            if (result.isPresent() && result.get() == delete) {
+                System.out.println("WARNING - DELETING " + repo);
+                if (cloneOrPullRepo(repo.sshUrl, repo.name)) {
+                    GithubApiWrapper githubApiWrapper = new GithubApiWrapper();
+                    githubApiWrapper.deleteRepository(repo);
+                    count++;
+                    progressBar.setProgress(count / total);
+                }
+            }
+
+        }
+        progressBar.setProgress(1.0);
     }
-  }
+
+    public boolean cloneOrPullRepo(String cloneUrl, String repoName) {
+        if (destinationDir == null) return false;
+
+        File destination = destinationDir.toPath().resolve(repoName).toFile();
+
+        try {
+            if (!destination.exists()) {
+
+                Git.cloneRepository()
+                    .setURI(cloneUrl)
+                    .setDirectory(
+                        destination
+                    )
+                    .setTransportConfigCallback(new App.SshCallback())
+                    .call();
+            } else {
+                Git repo = Git.open(destination);
+                repo
+                    .pull()
+                    .setTransportConfigCallback(new App.SshCallback())
+                    .call();
+            }
+            return true;
+        } catch (GitAPIException | IOException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
 }
